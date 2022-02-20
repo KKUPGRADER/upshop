2e890fda57d80a1286b106d60327e35713438196
Kanak <kkupgrader.fs@gmail.com>
2022-02-20 11:51:13 +0530

    .

--- script.js
+++ script.js
@@ -236,14 +236,4 @@ var shoppingCart = (function() {
   setTimeout(function() {
           cshare.setAttribute("data-mdb-original-title", "Link Copied!!!");
       }, 1000);
-      function send_handle(){
-
-        let num=document.getElementById("number").value;
       
-        let msg= document.getElementById("msg").value;
-      
-          let name= document.getElementById("name").value;
-        
-        var win = window.open(`https://wa.me/${num}?text=I%27m%20api%20msg%20hello%20${name}%20friend%20${msg}`, '_blank');
-       // win.focus();
-      }
